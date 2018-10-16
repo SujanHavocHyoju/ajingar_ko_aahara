a6e0ac54ddf1736afdbb3bfc8034757a5fa06dbd
Sujan HAVOC Hyoju <sujanhyoju@gmail.com>
2018-10-16 13:22:22 +0545

    Template Inheritence

--- pytonorm/src/restaurants/views.py
+++ pytonorm/src/restaurants/views.py
@@ -22,4 +22,15 @@ def home(request):
 
 	#return render(request, "base.html", {"html_var":"context variable", "boolean_value" : True, "result":"You can pass!", "num":num}) 
 
-	return render(request, "base.html", context) 
+	return render(request, "home.html", context) 
+
+def about(request):
+	
+	context = {
+			}
+	return render(request, "about.html", context) 
+
+def contact(request):
+	context = {
+			}
+	return render(request, "contact.html", context) 
