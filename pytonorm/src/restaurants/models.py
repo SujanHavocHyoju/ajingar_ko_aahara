1ff662be9e0b6f6e521d9602f1e9b6f59a9ea88e
Sujan HAVOC Hyoju <sujanhyoju@gmail.com>
2018-10-17 14:53:00 +0545

    DateTimeField

--- pytonorm/src/restaurants/models.py
+++ pytonorm/src/restaurants/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class RestaurantLocation(models.Model):
-	Name = models.CharField(max_length=120)
-	Description = models.TextField(default='Description Not Provided')
-	Location = models.TextField(max_length=120, null=True, blank=True)
+	name = models.CharField(max_length=120)
+	description = models.TextField(default='Description Not Provided')
+	location = models.CharField(max_length=120, null=True, blank=True)
+	category = models.CharField(max_length=120, null=True, blank=True)
+	added_date = models.DateTimeField(auto_now_add=True)
+	updated_date = models.DateTimeField(auto_now=True)
+	#my_date = models.DateTimeField(auto_now=False, auto_now_add=False)
