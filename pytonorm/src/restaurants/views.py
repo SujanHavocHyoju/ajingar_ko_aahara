c4b4076e303f29f91b88e804fac915a2deba4553
Sujan HAVOC Hyoju <sujanhyoju@gmail.com>
2018-11-03 16:06:42 +0545

    DetailView using rest_id instead of pk

--- pytonorm/src/restaurants/views.py
+++ pytonorm/src/restaurants/views.py
@@ -1,7 +1,7 @@
 import random
 from django.db.models import Q
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.views.generic import TemplateView, ListView, DetailView
 
@@ -43,6 +43,11 @@ class RestaurantDetailView(DetailView):
 		context = super(RestaurantDetailView, self).get_context_data(*args, **kwargs)
 		print(context)
 		return context
+
+	def get_object(self, *args, **kwargs):
+		rest_id = self.kwargs.get('rest_id')
+		obj = get_object_or_404(RestaurantLocation, id=rest_id)  #or pk = rest_id
+		return obj
 
 
 # class SearchRestaurantListView(ListView):
