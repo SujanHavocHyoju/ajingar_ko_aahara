eb7a8b6343d47036cc2430b5ce48ea24f1b4cfbd
Sujan HAVOC Hyoju <sujanhyoju@gmail.com>
2018-11-03 18:26:10 +0545

    Signal for unique slug

--- pytonorm/src/restaurants/models.py
+++ pytonorm/src/restaurants/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+from .utils import unique_slug_generator
 
 # Create your models here.
 class RestaurantLocation(models.Model):
@@ -17,4 +19,22 @@ class RestaurantLocation(models.Model):
 	@property
 	def title(self):
 		return self.name
+
+def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+	#print("Saving...")
+	#print(instance.added_date)
+	if not instance.slug:
+		instance.slug = unique_slug_generator(instance)
+
+# def rl_post_save_receiver(sender, instance, created, *args, **kwargs):
+# 	print("Saved.")
+# 	print(instance.added_date)
+# 	if not instance.slug:
+# 		instance.slug = unique_slug_generator(instance)
+# 		instance.save()
+
+
+pre_save.connect(rl_pre_save_receiver, sender=RestaurantLocation)
+
+#post_save.connect(rl_post_save_receiver, sender=RestaurantLocation)
 	
